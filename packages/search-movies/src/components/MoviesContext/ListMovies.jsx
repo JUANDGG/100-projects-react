e532373b469404c8eeac4ba1ecd9e06df19820dc
juandgg <juanguiza65@gmail.com>
2024-06-17 22:47:07 -0500

    feat: add mappedMovies struture

--- packages/search-movies/src/components/MoviesContext/ListMovies.jsx
+++ packages/search-movies/src/components/MoviesContext/ListMovies.jsx
@@ -1,13 +1,25 @@
 function ListMovies({ movies }) {
+    //evitar el contrato con la api para esto podremos mapear los datos
+
+    const mappedMovies =movies.map(movie=>({
+       id:movie["imdbID"],
+       title :movie["Title"],
+       poster :movie["Poster"],
+        year :movie["Year"]
+    }));
+
+    console.log(mappedMovies)
+
     return (
         <ul>
-            {movies.map(movie => (
+            {mappedMovies.map(movie => (
+                
                 movie.Poster !== "N/A" && (
-                    <li key={movie.imdbID}>
+                    <li key={movie.id}>
                         
-                        <p>{movie.Year}</p>
-                        <img src={movie.Poster} alt='no img' />
-                        <h3>{movie.Title}</h3>
+                        <p>{movie.title}</p>
+                        <img src={movie.poster} alt='no img' />
+                        <h3>{movie.year}</h3>
                     </li>
                 )
             ))}
